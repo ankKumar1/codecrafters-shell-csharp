5d95b58681a717879ea6b22653eb268e48063103
Ankit <iamankitkumar546@gmail.com>
2025-12-20 23:24:05 +0530

    added REPL

--- src/main.cs
+++ src/main.cs
@@ -3,8 +3,12 @@ class Program
     static void Main()
     {
         // TODO: Uncomment the code below to pass the first stage
-        Console.Write("$ ");
-        string command = Console.ReadLine();
-        Console.WriteLine($"{command}: command not found");
+        while (true)
+        {
+            Console.Write("$ ");
+            string command = Console.ReadLine();
+            Console.WriteLine($"{command}: command not found");
+        }
+
     }
 }
