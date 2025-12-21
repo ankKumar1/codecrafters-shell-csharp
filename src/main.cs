656a6c69c18feb88982ea39014da106563525205
Ankit <iamankitkumar546@gmail.com>
2025-12-21 21:33:10 +0530

    implemented echo

--- src/main.cs
+++ src/main.cs
@@ -13,8 +13,28 @@ class Program
                 break;
             }
 
-            Console.WriteLine($"{command}: command not found");
+            if (command != null)
+            {
+                int idx = command.IndexOf(' ');
+
+                string cmd = idx == -1 ? command : command.Substring(0, idx);
+                string args = idx == -1 ? string.Empty : command.Substring(idx + 1).Trim();
+
+                ExecuteCommand(cmd, args);
+            }
         }
 
+    }
+
+    public static void ExecuteCommand(string command, string args)
+    {
+        if (command == "echo")
+        {
+            Console.WriteLine(args);
+        }
+        else
+        {
+            Console.WriteLine($"{command}: command not found");
+        }
     }
 }
