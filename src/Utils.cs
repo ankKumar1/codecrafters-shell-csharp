2924cfdbe7133491f99599bb9e1c544b9299b8c6
Ankit <iamankitkumar546@gmail.com>
2026-01-07 00:06:00 +0530

    added namespaces

---
+++ src/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCrafters.Shell.Utilities;
+public static class Utils
+{
+    public static readonly string[] Builtins = ["echo", "type", "exit", "pwd", "cd"];
+    public static readonly string[] AutoCompleteBuiltins = ["echo", "exit"];
+}
+
