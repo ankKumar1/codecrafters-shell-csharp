04823e4489272edac447e34f11f816fd2b03290a
Ankit <akumar1@ph.iitr.ac.in>
2026-06-24 23:04:39 +0530

    codecrafters submit [skip ci]

--- src/Utilities/Utils.cs
+++ src/Utilities/Utils.cs
@@ -2,5 +2,5 @@ namespace CodeCrafters.Shell.Utilities;
 
 public static class Utils
 {
-    public static readonly string[] Builtins = ["echo", "type", "exit", "pwd", "cd", "history", "jobs", "complete"];
+    public static readonly string[] Builtins = ["echo", "type", "exit", "pwd", "cd", "history", "jobs", "complete", "declare"];
 }
